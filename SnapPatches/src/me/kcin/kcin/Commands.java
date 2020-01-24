62a465347114201bc75b1f951a8867a9f7f69e61
kcin2 <59776298+kcin2@users.noreply.github.com>
2020-01-24 10:38:12 -0500

    Update Commands.java

--- SnapPatches/src/me/kcin/kcin/Commands.java
+++ SnapPatches/src/me/kcin/kcin/Commands.java
@@ -1,50 +1,15 @@
-package me.kcin.kcin;
-
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-
-import net.minecraft.server.v1_12_R1.CommandExecute;
-
-public class Commands extends CommandExecute implements Listener, CommandExecutor {
-
-	Mainkcin plugin;
-
-	public Commands(Mainkcin pl) {
-		plugin = pl;
-	}
-
-	public String cmd1 = "kcin";
-	public String[] str = {"a","b","c","d","e","1","2","3","4","5","6","9"};
-	public int clr;
-	
-	
-	@Override
+@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
 		if (sender instanceof Player) {
 			Player player = ((Player) sender).getPlayer();
 			if (cmd.getName().equalsIgnoreCase(cmd1)) {
-				String savedPlotsWorld = plugin.getConfig().get("plotspatch.world").toString();
 				String savedSpawnWorld = plugin.getConfig().get("resistance-rework.world").toString();
-				boolean isplotspatch = plugin.getConfig().getBoolean("plotspatch-enabled");
 				boolean isresistrework = plugin.getConfig().getBoolean("resistance-rework-enabled");
 
 				if (sender.isOp() || sender.hasPermission("kcin.*")) {
 
 					if (args.length != 0) {
-						if (args[0].equalsIgnoreCase("setPlotsWorld")) {
-
-							plugin.getConfig().set("plotspatch.world", player.getWorld().getName().toString());
-							plugin.saveConfig();
-							sender.sendMessage(ChatColor.GREEN + "Plots patch world set to " + ChatColor.GOLD
-									+ player.getWorld().getName().toString());
-							return true;
-
-						} else
 
 						if (args[0].equalsIgnoreCase("setRRWorld")) {
 
@@ -63,8 +28,6 @@ public class Commands extends CommandExecute implements Listener, CommandExecuto
 					} else {
 
 						sender.sendMessage(ChatColor.GREEN + "----------------------------------------------------\n"
-								+ ChatColor.WHITE + "Plots tp patch enabled: " + ChatColor.GREEN + isplotspatch + "\n"
-								+ ChatColor.WHITE + "Plots tp patch world: " + ChatColor.GREEN + savedPlotsWorld + "\n"
 								+ ChatColor.WHITE + "Resistance rework enabled: " + ChatColor.GREEN + isresistrework
 								+ "\n" + ChatColor.WHITE + "Resistance rework world: " + ChatColor.GREEN
 								+ savedSpawnWorld + "\n----------------------------------------------------");
@@ -72,12 +35,14 @@ public class Commands extends CommandExecute implements Listener, CommandExecuto
 					}
 
 				} else {
-					if (clr > 11) {clr = 0;}
+					if (clr > 11) {
+						clr = 0;
+					}
 					String a = str[clr];
-					sender.sendMessage("§" + a + " ok");
+					sender.sendMessage("§" + a + "ok");
 					clr++;
 					return true;
-					
+
 				}
 
 			}
@@ -90,5 +55,3 @@ public class Commands extends CommandExecute implements Listener, CommandExecuto
 		}
 		return false;
 	}
-
-}
